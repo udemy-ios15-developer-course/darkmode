0dd700d93f240ed842db5e6c4eedb01567729162
Chris Hand <cj.hand@gmail.com>
2022-09-01 21:35:43 -0700

    Create basic UI

--- darkmode/ViewController.swift
+++ darkmode/ViewController.swift
@@ -9,6 +9,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var button: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
